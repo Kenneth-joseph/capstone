8af11ceaee23cc326366683fb8a5105c5c15522e
trevor ngugi <ngugitrevor@gmail.com>
2020-02-28 07:41:45 +0300

    add sign up form

--- trevor/urls.py
+++ trevor/urls.py
@@ -5,4 +5,6 @@ urlpatterns=[
     url('^$',views.signin,name = 'sign_in'),
     url(r'^postsign/',views.postsign),
     url(r'^logoout/',views.logout,name='logout'),
+    url(r'^signUp/',views.signUp,name='sign_up'),
+    url(r'^postsignup/',views.postsignup,name='postsignup'),
 ]
--- trevor/views.py
+++ trevor/views.py
@@ -15,6 +15,7 @@ config={
 }
 firebase = pyrebase.initialize_app(config)
 authe = firebase.auth()
+database=firebase.database()#initialising the database
  
 
 # Create your views here.
@@ -41,4 +42,26 @@ def postsign(request):
 
 def logout(request):
     auth.logout(request)
+    return render(request,'signin.html')
+
+def signUp(request):
+
+    return render(request,'signUp.html')
+
+def postsignup(request):
+    name=request.POST.get('name')
+    email=request.POST.get('email')    
+    passw=request.POST.get('pass')
+
+    try:
+        user = authe.create_user_with_email_and_password(email, passw)#creating the account
+
+    except:
+        message='unable to create account try again'
+        return render(request,'signin.html',{'message':message})
+        uid=user['localId']#getting the local id which is unique for everyone
+        
+    data={'name':name,"status":"1"}#status for enabling this account data for getting the name vaule
+    database.child('users').child(uid).child('details').set(data)# create constructor called child which enables many users to be entered and it collects the uid and details
+
     return render(request,'signin.html')
